01669505ea71a388bd7fff72dd3446ab640abb77
Dang Hai <pecheule34713@gmail.com>
2025-09-17 01:47:43 +0700

    java(lc-cf): update mod

---
+++ java/src/main/java/org/noxhalley/cf/ac/KefaAndFirstSteps_580A.java
@@ -0,0 +1,45 @@
+package org.noxhalley.cf.ac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class KefaAndFirstSteps_580A {
+
+    public static void main(String[] args) {
+        Reader in = new Reader();
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; ++i) arr[i] = in.nextInt();
+        int maxLength = 1;
+        int res = maxLength;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] >= arr[i-1]) maxLength++;
+            else {
+                res = Math.max(res, maxLength);
+                maxLength = 1;
+            }
+        }
+        res = Math.max(res, maxLength);
+        System.out.println(res);
+    }
+
+    private static class Reader {
+        StringTokenizer st;
+        BufferedReader bf;
+        Reader() {this.bf = new BufferedReader(new InputStreamReader(System.in));}
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(bf.readLine());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() {return Integer.parseInt(next());}
+    }
+}
